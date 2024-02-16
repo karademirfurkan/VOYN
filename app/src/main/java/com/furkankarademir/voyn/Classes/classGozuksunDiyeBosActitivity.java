a3816b7a0e0531b4da24176d00146892295cf7fb
MuhammedEmirEren <emir4200@hotmail.com>
2024-02-16 16:33:00 +0300

    new class (java)

---
+++ app/src/main/java/com/furkankarademir/voyn/Classes/classGozuksunDiyeBosActitivity.java
@@ -0,0 +1,16 @@
+package com.furkankarademir.voyn.Classes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.furkankarademir.voyn.R;
+
+public class classGozuksunDiyeBosActitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_gozuksun_diye_bos_actitivity);
+    }
+}
