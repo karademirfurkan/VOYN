b0d6a77d7ae19fd4e12b93662dc5c9e67c217f82
MuhammedEmirEren <emir4200@hotmail.com>
2024-02-24 23:11:03 +0300

    I tried to do all of them again manuelly

--- app/src/main/java/com/furkankarademir/voyn/MenuPage.java
+++ app/src/main/java/com/furkankarademir/voyn/MenuPage.java
@@ -1,14 +1,57 @@
 package com.furkankarademir.voyn;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import android.view.View;
+import android.widget.FrameLayout;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import android.os.Bundle;
 
+import com.furkankarademir.voyn.databinding.ActivityMainBinding;
+
+
 public class MenuPage extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu_page);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemid())
+            {
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+                case R.id.messages:
+                    replaceFragment(new MessagesFragment());
+                    break;
+            }
+
+
+
+
+            return true;
+        });
+
+
+        private void replaceFragment(Fragment fragment)
+        {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.constraint_layout, fragment);
+            fragmentTransaction.commit();
+
+        }
     }
 }
