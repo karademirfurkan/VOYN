e1f08a856f3d9a8893cdd13688e8c41756649f85
MuhammedEmirEren <emir4200@hotmail.com>
2024-02-24 23:21:57 +0300

    Unnecessary codes are deleted

--- app/src/main/java/com/furkankarademir/voyn/ProfileFragment.java
+++ app/src/main/java/com/furkankarademir/voyn/ProfileFragment.java
@@ -8,11 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProfileFragment extends Fragment {
     public ProfileFragment() {
         // Required empty public constructor
