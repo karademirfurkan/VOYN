4550e87a11311dc80224d0e5e0299d8d0cca923b
MuhammedEmirEren <emir4200@hotmail.com>
2024-03-30 19:16:40 +0300

    some improvements in invitation class

--- app/src/main/java/com/furkankarademir/voyn/ParentClassesForActivity/Invitation.java
+++ app/src/main/java/com/furkankarademir/voyn/ParentClassesForActivity/Invitation.java
@@ -1,6 +1,33 @@
 package com.furkankarademir.voyn.ParentClassesForActivity;
 
+import com.furkankarademir.voyn.Classes.User;
+
 public abstract class Invitation {
     private String ownerID;
     private String attendeeID;
+    private String activityID;
+
+    private User owner;
+
+    public Invitation(String ownerID, String attendeeID, String activityID)
+    {
+        this.ownerID = ownerID;
+        this.attendeeID = attendeeID;
+        this.activityID = activityID;
+    }
+
+    public String getOwnerID()
+    {
+        return ownerID;
+    }
+
+    public String getAttendeeID()
+    {
+        return attendeeID;
+    }
+
+    public String getActivityID()
+    {
+        return activityID;
+    }
 }
