eae2d4cfb68b32a1a199a27de1fb23f53994f290
Furkan Karademir <furkan.karademir@ug.bilkent.edu.tr>
2024-04-02 01:34:35 +0300

    Chat user list is done

---
+++ app/src/main/java/com/furkankarademir/voyn/Chat/ChatUserListAdapter.java
@@ -0,0 +1,80 @@
+package com.furkankarademir.voyn.Chat;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.furkankarademir.voyn.Classes.User;
+import com.furkankarademir.voyn.MenuPage;
+import com.furkankarademir.voyn.Transportation.TransportationAdapter;
+import com.furkankarademir.voyn.Transportation.TransportationDetailActivity;
+import com.furkankarademir.voyn.databinding.ChattedUsersListRowBinding;
+import com.furkankarademir.voyn.databinding.RecyclerTransportationRowBinding;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ChatUserListAdapter extends RecyclerView.Adapter<ChatUserListAdapter.ChatUserListHolder> {
+
+    private ArrayList<User> users;
+    public  ChatUserListAdapter(ArrayList<User> users)
+    {
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public ChatUserListHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ChattedUsersListRowBinding chattedUsersListRowBinding = ChattedUsersListRowBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        return new ChatUserListAdapter.ChatUserListHolder(chattedUsersListRowBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChatUserListHolder holder, int position)
+    {
+        holder.binding.name.setText(users.get(position).getName());
+        holder.binding.surname.setText(users.get(position).getSurname());
+        holder.binding.mail.setText(users.get(position).getMail());
+        holder.binding.password.setText(users.get(position).getPassword());
+
+
+
+        //should be updated
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(holder.itemView.getContext(), MenuPage.class);
+                Toast.makeText(v.getContext(), "be patient, we did not prepared this page yet", Toast.LENGTH_LONG).show();
+                holder.itemView.getContext().startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+
+    public class ChatUserListHolder extends RecyclerView.ViewHolder
+    {
+        private ChattedUsersListRowBinding binding;
+        public ChatUserListHolder(ChattedUsersListRowBinding binding)
+        {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+
+    public void updateData(ArrayList<User> users) {
+        this.users = users;
+        notifyDataSetChanged();
+    }
+
+}
--- app/src/main/java/com/furkankarademir/voyn/MessagesFragment.java
+++ app/src/main/java/com/furkankarademir/voyn/MessagesFragment.java
@@ -1,20 +1,61 @@
 package com.furkankarademir.voyn;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MessagesFragment extends Fragment {
+import com.furkankarademir.voyn.Chat.ChatUserListAdapter;
+import com.furkankarademir.voyn.Classes.User;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+
+public class MessagesFragment extends Fragment {
+    private FirebaseAuth auth;
+    private FirebaseFirestore db;
+
+    private ChatUserListAdapter chatUserListAdapter;
+
+    private ArrayList<User> users;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        users = new ArrayList<>();
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        makeArrayList();
     }
+
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+
+        RecyclerView rcv = view.findViewById(R.id.rcv);
+        rcv.setLayoutManager(new LinearLayoutManager(getContext()));
+        chatUserListAdapter= new ChatUserListAdapter(users);
+        rcv.setAdapter(chatUserListAdapter);
+
+    }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,4 +63,35 @@ public class MessagesFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_messages, container, false);
     }
+
+    public void makeArrayList()
+    {
+        db.collection("Users").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                for (QueryDocumentSnapshot snapshot : queryDocumentSnapshots)
+                {
+                    String name = snapshot.getString("name");
+                    String surname = snapshot.getString("surname");
+
+                    User user = new User(name, surname);
+                    users.add(user);
+                }
+
+                chatUserListAdapter.updateData(users);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                System.out.println("message fragment olmadı");
+            }
+        });
+    }
+
+    public void userSelected(View view)
+    {
+        Intent intent = new Intent(getContext(), MenuPage.class);
+        startActivity(intent);
+    }
+
 }
