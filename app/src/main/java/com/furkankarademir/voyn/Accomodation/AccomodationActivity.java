b0576e1354e40fd5b1bf9a0b028a21065a695c29
Furkan Karademir <furkan.karademir@ug.bilkent.edu.tr>
2024-04-01 20:31:29 +0300

    Accomodation activity is done

--- app/src/main/java/com/furkankarademir/voyn/Accomodation/AccomodationActivity.java
+++ app/src/main/java/com/furkankarademir/voyn/Accomodation/AccomodationActivity.java
@@ -1,16 +1,91 @@
 package com.furkankarademir.voyn.Accomodation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.furkankarademir.voyn.R;
+import com.furkankarademir.voyn.Transportation.AddTransportationActivity;
+import com.furkankarademir.voyn.databinding.ActivityAccomodationBinding;
+import com.furkankarademir.voyn.databinding.ActivityAddAccomodationBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 
 public class AccomodationActivity extends AppCompatActivity {
+    private ActivityAccomodationBinding binding;
+
+    private FirebaseFirestore db;
+    private ArrayList<HashMap<String, Object>> accomodationActivities;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_accomodation);
+        binding = ActivityAccomodationBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        db = FirebaseFirestore.getInstance();
+        accomodationActivities = new ArrayList<>();
+
+
+        makeArrayList();
+
+
+    }
+
+    public void makeArrayList()
+    {
+        db.collection("accomodations").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots)
+                {
+                    HashMap<String, Object> accomodation = (HashMap<String, Object>) documentSnapshot.getData();
+                    accomodationActivities.add(accomodation);
+                }
+
+
+                accomodationActivities.sort((o1, o2) -> {
+                    if(o1.get("date") == null || o2.get("date") == null)
+                        return 0;
+                    String date1 = (String) o1.get("date");
+                    String date2 = (String) o2.get("date");
+
+                    SimpleDateFormat format = new SimpleDateFormat("yyyy.MM.dd");
+
+                    try {
+                        Date dateObj1 = format.parse(date1);
+                        Date dateObj2 = format.parse(date2);
+                        return dateObj1.compareTo(dateObj2);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                        return 0;
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(AccomodationActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void addAccomodationButton(View view) {
+        Intent intent = new Intent(this, AddTransportationActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/furkankarademir/voyn/Accomodation/AddAccomodationActivity.java
+++ app/src/main/java/com/furkankarademir/voyn/Accomodation/AddAccomodationActivity.java
@@ -2,16 +2,23 @@ package com.furkankarademir.voyn.Accomodation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.furkankarademir.voyn.R;
+import com.furkankarademir.voyn.databinding.ActivityAddAccomodationBinding;
+import com.furkankarademir.voyn.databinding.ActivityAddTransportationBinding;
 
 public class AddAccomodationActivity extends AppCompatActivity {
+    private ActivityAddAccomodationBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_accomodation);
+        binding = ActivityAddAccomodationBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 
     public void openActivityButtonClicked()
@@ -21,6 +28,8 @@ public class AddAccomodationActivity extends AppCompatActivity {
 
     public void cancelButtonClicked()
     {
-        
+        Intent intent = new Intent(AddAccomodationActivity.this, AccomodationActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
