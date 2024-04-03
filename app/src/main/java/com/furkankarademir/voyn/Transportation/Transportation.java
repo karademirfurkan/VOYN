ca7cf1b4ef60255823548e700f90eacae6ea8d9d
MuhammedEmirEren <emir4200@hotmail.com>
2024-04-03 18:55:40 +0300

    onSuccess listener changed

--- app/src/main/java/com/furkankarademir/voyn/Transportation/Transportation.java
+++ app/src/main/java/com/furkankarademir/voyn/Transportation/Transportation.java
@@ -12,6 +12,7 @@ import com.furkankarademir.voyn.ProfileClasses.Profile;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -66,20 +67,20 @@ public class Transportation extends Activity{
 
         db.collection("transportations")
                 .add(transportation)
-                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
-                    public void onComplete(@NonNull Task<DocumentReference> task) {
-                        if (task.isSuccessful()) {
-                            DocumentReference documentReference = task.getResult();
-                            System.out.println("her şey ejkendi");
-                            Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                            thisActivityID[0] = documentReference.getId();
-                            System.out.println("bu da id" + thisActivityID[0]);
-                        } else {
-                            Exception e = task.getException();
-                            System.out.println("olmadı beee");
-                            Log.w(TAG, "Error adding document", e);
-                        }
+                    public void onSuccess(DocumentReference documentReference) {
+                        System.out.println("her sey eklendi");
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                        thisActivityID[0] = documentReference.getId();
+                        System.out.println("bu da id" + thisActivityID[0]);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        System.out.println("olmadı beee");
+                        Log.w(TAG, "Error adding document", e);
                     }
                 });
         System.out.println("bu da id 2.deneme" + thisActivityID[0]);
