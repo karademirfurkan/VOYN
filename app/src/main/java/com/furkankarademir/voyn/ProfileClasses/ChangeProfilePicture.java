239b4256d7f4ec54abdc610f69ced33994e520a2
Alp Eren Köken <eren.koken@ug.bilkent.edu.tr>
2024-04-18 20:51:37 +0300

    Edit Profile

---
+++ app/src/main/java/com/furkankarademir/voyn/ProfileClasses/ChangeProfilePicture.java
@@ -0,0 +1,13 @@
+package com.furkankarademir.voyn.ProfileClasses;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChangeProfilePicture extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
