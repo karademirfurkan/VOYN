eac8dd83813052758e943c0e957f6a45e5312bda
MuhammedEmirEren <emir4200@hotmail.com>
2024-04-19 21:05:50 +0300

    EditProfile intent and log out button is done

--- app/src/main/java/com/furkankarademir/voyn/ProfileFragment.java
+++ app/src/main/java/com/furkankarademir/voyn/ProfileFragment.java
@@ -1,16 +1,23 @@
 package com.furkankarademir.voyn;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.furkankarademir.voyn.ProfileClasses.EditProfile;
 import com.furkankarademir.voyn.databinding.ActivitySignUpPageBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileFragment extends Fragment {
+
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -27,5 +34,29 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button editProfile = view.findViewById(R.id.editProfileButton);
+        editProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), EditProfile.class);
+                startActivity(intent);
+            }
+        });
+
+        Button logOut = view.findViewById(R.id.logOutButton);
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
