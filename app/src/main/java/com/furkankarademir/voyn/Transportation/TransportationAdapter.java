17fea7d5f103112ac393ed0366196ffeb3eb0b59
Furkan Karademir <furkan.karademir@ug.bilkent.edu.tr>
2024-05-01 02:31:57 +0300

    showing activities red in case of insufficient star is done

--- app/src/main/java/com/furkankarademir/voyn/Transportation/TransportationAdapter.java
+++ app/src/main/java/com/furkankarademir/voyn/Transportation/TransportationAdapter.java
@@ -1,34 +1,52 @@
 package com.furkankarademir.voyn.Transportation;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.ColorRes;
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.furkankarademir.voyn.Classes.User;
+import com.furkankarademir.voyn.R;
 import com.furkankarademir.voyn.databinding.RecyclerTransportationRowBinding;
 import com.furkankarademir.voyn.myactivitiesclasses.myTransportationActivityDetails;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.type.Color;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
+
+
+
+
 public class TransportationAdapter extends RecyclerView.Adapter<TransportationAdapter.TransportationHolder> {
-
     private ArrayList<HashMap<String, Object>> transportationActivities;
-
+    private FirebaseAuth auth;
+    private FirebaseFirestore db;
+    private User user;
     private int transportationAdapterOption;
     public  TransportationAdapter(ArrayList<HashMap<String, Object>> transportationActivities, int i)
     {
         this.transportationActivities = transportationActivities;
         transportationAdapterOption = i;
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
     }
 
 
@@ -48,7 +66,7 @@ public class TransportationAdapter extends RecyclerView.Adapter<TransportationAd
     }
 
     @Override
-    public void onBindViewHolder(@NonNull TransportationHolder holder, int position)
+    public void onBindViewHolder(@NonNull TransportationHolder holder, @SuppressLint("RecyclerView") int position)
     {
         HashMap<String, Object> currentActivity = transportationActivities.get(position);
         if(currentActivity == null)
@@ -60,6 +78,7 @@ public class TransportationAdapter extends RecyclerView.Adapter<TransportationAd
         String date = (String) transportationActivities.get(position).get("date");
         String destination = (String) transportationActivities.get(position).get("destination");
         String personLimit = transportationActivities.get(position).get("seats").toString();
+        double minStar = Double.parseDouble(transportationActivities.get(position).get("minStar").toString());
 
         holder.binding.time.setText(time);
         holder.binding.date.setText(date);
@@ -67,29 +86,52 @@ public class TransportationAdapter extends RecyclerView.Adapter<TransportationAd
         holder.binding.whereTo.setText(destination);
         holder.binding.availableSeats.setText(personLimit);
 
+        //----------------------
+        DocumentReference docRef = db.collection("Users").document(auth.getUid());
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if(documentSnapshot.exists())
+                {
+                    user = documentSnapshot.toObject(User.class);
 
-        if(transportationAdapterOption == 0)
-        {
-            holder.itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(holder.itemView.getContext(), TransportationDetailActivity.class);
-                    intent.putExtra("transportation", transportationActivities.get(position));
-                    holder.itemView.getContext().startActivity(intent);
+                    if (user.getStar() < minStar) {
+                        //holder.binding.bigLinearLayout.setBackgroundColor(ContextCompat.getColor(holder.itemView.getContext(), R.color.red));
+                        holder.binding.bigLinearLayout.setBackground(ContextCompat.getDrawable(holder.itemView.getContext(), R.drawable.red_row_view));
+
+                    } else {
+                        if(transportationAdapterOption == 0)
+                        {
+                            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Intent intent = new Intent(holder.itemView.getContext(), TransportationDetailActivity.class);
+                                    intent.putExtra("transportation", transportationActivities.get(position));
+                                    holder.itemView.getContext().startActivity(intent);
+                                }
+                            });
+                        }
+                        else
+                        {
+                            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Intent intent = new Intent(holder.itemView.getContext(), myTransportationActivityDetails.class);
+                                    intent.putExtra("transportation", transportationActivities.get(position));
+                                    holder.itemView.getContext().startActivity(intent);
+                                }
+                            });
+                        }
+                    }
                 }
-            });
-        }
-        else
-        {
-            holder.itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(holder.itemView.getContext(), myTransportationActivityDetails.class);
-                    intent.putExtra("transportation", transportationActivities.get(position));
-                    holder.itemView.getContext().startActivity(intent);
-                }
-            });
-        }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+            }
+        });
+        //-------------------------------
+
     }
 
     @Override
