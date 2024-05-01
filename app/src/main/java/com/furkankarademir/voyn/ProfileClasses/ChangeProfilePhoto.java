b5af8dfd9734b100e4a1bf986ec44914e34aa105
MuhammedEmirEren <emir4200@hotmail.com>
2024-05-01 19:22:36 +0300

    Change profile picture xml

--- app/src/main/java/com/furkankarademir/voyn/ProfileClasses/ChangeProfilePhoto.java
+++ app/src/main/java/com/furkankarademir/voyn/ProfileClasses/ChangeProfilePhoto.java
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.furkankarademir.voyn.databinding.ChangeProfilePhotoBinding;
+
+
 import com.furkankarademir.voyn.R;
 
 public class ChangeProfilePhoto extends AppCompatActivity {
 
+    private binding ChangeProfilePhotoBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_change_profile_photo);
+        ChangeProfilePhotoBinding = ChangeProfilePhotoBinding.inflate(getLayoutInflater());
+        setContentView(ChangeProfilePhotoBinding.getRoot());
+
     }
 }
