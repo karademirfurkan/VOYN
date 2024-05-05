da0df2ec9aab995bf2763090299ec3bc949c53da
Alp Eren Köken <eren.koken@ug.bilkent.edu.tr>
2024-05-06 01:09:23 +0300

    AccommodationDetail has completed and some bugs have fixed

--- app/src/main/java/com/furkankarademir/voyn/Accomodation/AccomodationDetailActivity.java
+++ app/src/main/java/com/furkankarademir/voyn/Accomodation/AccomodationDetailActivity.java
@@ -1,16 +1,142 @@
 package com.furkankarademir.voyn.Accomodation;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
+import com.furkankarademir.voyn.Chat.Chat;
+import com.furkankarademir.voyn.Chat.ChatInBetweenPage;
+import com.furkankarademir.voyn.Chat.Message;
 import com.furkankarademir.voyn.R;
+import com.furkankarademir.voyn.Sport.SportsDetailActivity;
+import com.furkankarademir.voyn.databinding.ActivityAccomodationDetailBinding;
+import com.furkankarademir.voyn.databinding.ActivitySportsDetailBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class AccomodationDetailActivity extends AppCompatActivity {
 
+    private ActivityAccomodationDetailBinding binding;
+
+    private HashMap<String, Object> accommodation;
+
+    private FirebaseFirestore db;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_accomodation_detail);
+        binding = ActivityAccomodationDetailBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        Intent intent = getIntent();
+        HashMap<String, Object> accommodationMap = (HashMap<String, Object>) intent.getSerializableExtra("accommodation");
+        accommodation = accommodationMap;
+
+        System.out.println(accommodation.get("creatorUserID"));
+
+        String name = (String) accommodationMap.get("name");
+        binding.nameInfo.setText(name);
+        binding.genderInfo.setText((String) accommodationMap.get("gender"));//it has written wrong in the xml part
+        binding.timeInfo.setText((String) accommodationMap.get("place")); //it has written wrong in the xml part
+        binding.dateInfo.setText((String) accommodationMap.get("date"));
+        binding.timeInfo.setText((String) accommodationMap.get("time"));
+        binding.inhabitantslimitInfo.setText(accommodationMap.get("numberOfPlayers").toString());
+        ArrayList<String> invited = (ArrayList<String>) accommodationMap.get("invited");
+        if (invited != null) {
+            // binding.deneme.setText(invited.toString());
+        }
     }
+    public void sendMessageButtonClicked(View view)
+    {
+        db.collection("Chat")
+                .whereEqualTo("firstUserId", auth.getUid())
+                .whereEqualTo("secondUserId", accommodation.get("creatorUserID").toString())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots)
+                    {
+                        if (queryDocumentSnapshots.isEmpty()) {
+                            // Chat doesn't exist, create a new chat
+                            ArrayList<Message> messagesInBetween = new ArrayList<>();
+                            Chat newChat = new Chat(auth.getUid().toString(),accommodation.get("creatorUserID").toString(),messagesInBetween);
+                            db.collection("Chat").add(newChat).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                                    Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                                    Toast.makeText(AccomodationDetailActivity.this, "new chat added", Toast.LENGTH_LONG).show();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.w(TAG, "Error adding document", e);
+                                }
+                            });
+                        }
+                        else
+                        {
+                            Toast.makeText(AccomodationDetailActivity.this, "same chat", Toast.LENGTH_LONG).show();
+                        }
+
+                        Intent intent = new Intent(AccomodationDetailActivity.this, ChatInBetweenPage.class);
+                        intent.putExtra("sport", accommodation);
+                        startActivity(intent);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, "Error checking chat existence", e);
+                    }
+                });
+
+
+    }
+    public void sendInvitationButtonClicked(View view)
+    {
+        binding.sendInvitation.setVisibility(View.INVISIBLE);
+        ArrayList<String> invited = (ArrayList<String>) accommodation.get("invited");
+        if (invited == null) {
+            invited = new ArrayList<>();
+        }
+
+        // Add the current user's ID to the invited ArrayList
+        invited.add(auth.getUid());
+
+        accommodation.put("invited", invited);
+
+        db.collection("accommodation").document(accommodation.get("documentId").toString())
+                .set(accommodation)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully updated!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error updating document", e);
+                    }
+                });
+    }
+
 }
