81188d2a3357373a901a8b91e6a8aea7043208eb
Alp Eren Köken <eren.koken@ug.bilkent.edu.tr>
2024-05-06 00:37:35 +0300

    myaccomodationdetail and mysportsdetail
    myaccomodationdetail and mysportsdetail have completed

--- app/src/main/java/com/furkankarademir/voyn/myactivitiesclasses/myAccommodationActivityDetails.java
+++ app/src/main/java/com/furkankarademir/voyn/myactivitiesclasses/myAccommodationActivityDetails.java
@@ -1,16 +1,65 @@
 package com.furkankarademir.voyn.myactivitiesclasses;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.furkankarademir.voyn.R;
+import com.furkankarademir.voyn.databinding.ActivityMyAccommodationDetailsBinding;
+import com.furkankarademir.voyn.databinding.ActivityMyTransportationDetailsBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class myAccommodationActivityDetails extends AppCompatActivity {
+
+    private HashMap<String, Object> accommodation;
+
+    private ActivityMyAccommodationDetailsBinding binding;
+
+
+    private ArrayList<String> acceptedUserList;
+
+    private FirebaseFirestore db;
+    private FirebaseAuth auth;
+    private FirebaseUser currentUser;
+
+    private HashMap<String, Object> accommodationMap;
+
+    private AcceptedIncomingInvitationsAdapter acceptedIncomingInvitationsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_accommodation_details);
+        binding = ActivityMyAccommodationDetailsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Intent intent = getIntent();
+        accommodationMap = (HashMap<String, Object>) intent.getSerializableExtra("accommodation");
+        accommodation = accommodationMap;
+
+        acceptedUserList = new ArrayList<>();
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+        currentUser = auth.getCurrentUser();
+
+        acceptedUserList = (ArrayList<String>) accommodationMap.get("participantsId");
+
+        binding.recyclerView3.setLayoutManager(new LinearLayoutManager(myAccommodationActivityDetails.this));
+        acceptedIncomingInvitationsAdapter = new AcceptedIncomingInvitationsAdapter(acceptedUserList);
+        binding.recyclerView3.setAdapter(acceptedIncomingInvitationsAdapter);
+    }
+
+    public void incomingInvitationButtonClicked(View view) {
+        Intent intent = new Intent(this, WaitingIncomingInvitations.class);
+        intent.putExtra("accommodation", accommodation);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/furkankarademir/voyn/myactivitiesclasses/mySportsActivityDetails.java
+++ app/src/main/java/com/furkankarademir/voyn/myactivitiesclasses/mySportsActivityDetails.java
@@ -1,16 +1,65 @@
 package com.furkankarademir.voyn.myactivitiesclasses;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.furkankarademir.voyn.R;
+import com.furkankarademir.voyn.databinding.ActivityMySportsDetailsBinding;
+import com.furkankarademir.voyn.databinding.ActivityMyTransportationDetailsBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class mySportsActivityDetails extends AppCompatActivity {
+
+    private HashMap<String, Object> sports;
+
+    private ActivityMySportsDetailsBinding binding;
+
+
+    private ArrayList<String> acceptedUserList;
+
+    private FirebaseFirestore db;
+    private FirebaseAuth auth;
+    private FirebaseUser currentUser;
+
+    private HashMap<String, Object> sportsMap;
+
+    private AcceptedIncomingInvitationsAdapter acceptedIncomingInvitationsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_sports_details);
+        binding = ActivityMySportsDetailsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Intent intent = getIntent();
+        sportsMap = (HashMap<String, Object>) intent.getSerializableExtra("sports");
+        sports = sportsMap;
+
+        acceptedUserList = new ArrayList<>();
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+        currentUser = auth.getCurrentUser();
+
+        acceptedUserList = (ArrayList<String>) sportsMap.get("participantsId");
+
+        binding.recyclerView3.setLayoutManager(new LinearLayoutManager(mySportsActivityDetails.this));
+        acceptedIncomingInvitationsAdapter = new AcceptedIncomingInvitationsAdapter(acceptedUserList);
+        binding.recyclerView3.setAdapter(acceptedIncomingInvitationsAdapter);
+    }
+
+    public void incomingInvitationButtonClicked(View view) {
+        Intent intent = new Intent(this, WaitingIncomingInvitations.class);
+        intent.putExtra("sports", sports);
+        startActivity(intent);
     }
 }
