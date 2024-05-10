5d937d172309662b0557b70113aa295a43b71bee
MuhammedEmirEren <emir4200@hotmail.com>
2024-05-10 11:53:30 +0300

    Waiting incoming invitations is done for accommo and sports
    Missing parts: Some filtering, red part for sport, invititatin accepting for sports and accommodation

--- app/src/main/java/com/furkankarademir/voyn/myactivitiesclasses/WaitingIncomingInvitations.java
+++ app/src/main/java/com/furkankarademir/voyn/myactivitiesclasses/WaitingIncomingInvitations.java
@@ -19,6 +19,10 @@ public class WaitingIncomingInvitations extends AppCompatActivity {
 
     private HashMap<String, Object> transportation;
 
+    private HashMap<String, Object> accommodation;
+
+    private HashMap<String, Object> sport;
+
     private ActivityWaitingIncomingInvitationsBinding binding;
 
     @Override
@@ -30,20 +34,54 @@ public class WaitingIncomingInvitations extends AppCompatActivity {
 
 
         Intent intent = getIntent();
-        HashMap<String, Object> transportationMap = (HashMap<String, Object>) intent.getSerializableExtra("transportation");
-        transportation = transportationMap;
-
-        ArrayList<String> participants = (ArrayList<String>) transportation.get("participantsId");
-        boolean isFull = false;
-        if(participants.size() == Integer.parseInt(transportation.get("seats").toString()))
+        if(intent.getSerializableExtra("transportation") != null)
         {
-            isFull = true;
-        }
+            HashMap<String, Object> transportationMap = (HashMap<String, Object>) intent.getSerializableExtra("transportation");
+            transportation = transportationMap;
 
-        binding.recyclerView4.setLayoutManager(new LinearLayoutManager(WaitingIncomingInvitations.this));
-        ArrayList<String> incomingInvitations = (ArrayList<String>) transportation.get("invited");
-        IncomingInvitationsAdapter incomingInvitationsAdapter = new IncomingInvitationsAdapter(incomingInvitations, transportation.get("documentId").toString(), isFull);
-        binding.recyclerView4.setAdapter(incomingInvitationsAdapter);
+            ArrayList<String> participants = (ArrayList<String>) transportation.get("participantsId");
+            boolean isFull = false;
+            if (participants.size() == Integer.parseInt(transportation.get("seats").toString())) {
+                isFull = true;
+            }
+
+            binding.recyclerView4.setLayoutManager(new LinearLayoutManager(WaitingIncomingInvitations.this));
+            ArrayList<String> incomingInvitations = (ArrayList<String>) transportation.get("invited");
+            IncomingInvitationsAdapter incomingInvitationsAdapter = new IncomingInvitationsAdapter(incomingInvitations, transportation.get("documentId").toString(), isFull);
+            binding.recyclerView4.setAdapter(incomingInvitationsAdapter);
+        }
+        else if (intent.getSerializableExtra("accommodation") != null)
+        {
+            HashMap<String, Object> accommodationMap = (HashMap<String, Object>) intent.getSerializableExtra("accommodation");
+            accommodation = accommodationMap;
+
+            ArrayList<String> participants = (ArrayList<String>) accommodation.get("participantsId");
+            boolean isFull = false;
+            if (participants.size() == Integer.parseInt(accommodation.get("numberOfInhabitants").toString())) {
+                isFull = true;
+            }
+
+            binding.recyclerView4.setLayoutManager(new LinearLayoutManager(WaitingIncomingInvitations.this));
+            ArrayList<String> incomingInvitations = (ArrayList<String>) accommodation.get("invited");
+            IncomingInvitationsAdapter incomingInvitationsAdapter = new IncomingInvitationsAdapter(incomingInvitations, accommodation.get("documentId").toString(), isFull);
+            binding.recyclerView4.setAdapter(incomingInvitationsAdapter);
+        }
+        else if (intent.getSerializableExtra("sports") != null)
+        {
+            HashMap<String, Object> sportsMap = (HashMap<String, Object>) intent.getSerializableExtra("sports");
+            sport = sportsMap;
+
+            ArrayList<String> participants = (ArrayList<String>) sport.get("participantsId");
+            boolean isFull = false;
+            if (participants.size() == Integer.parseInt(sport.get("numberOfPlayers").toString())) {
+                isFull = true;
+            }
+
+            binding.recyclerView4.setLayoutManager(new LinearLayoutManager(WaitingIncomingInvitations.this));
+            ArrayList<String> incomingInvitations = (ArrayList<String>) sport.get("invited");
+            IncomingInvitationsAdapter incomingInvitationsAdapter = new IncomingInvitationsAdapter(incomingInvitations, sport.get("documentId").toString(), isFull);
+            binding.recyclerView4.setAdapter(incomingInvitationsAdapter);
+        }
 
     }
 }
